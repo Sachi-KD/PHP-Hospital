6a15028689cc2c884a2534b71d68b87d2537a993
Sachi <sachithra@efuturesworld.com>
2024-06-03 11:05:54 +0530

    admin done

--- channel/admin/doctors.php
+++ channel/admin/doctors.php
@@ -215,7 +215,7 @@
                                     <td colspan="4">
                                     <br><br><br><br>
                                     <center>
-                                    <img src="../img/notfound.svg" width="25%">
+                                    <img src="../img/notfound.png" width="5%">
                                     
                                     <br>
                                     <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
